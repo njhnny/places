67ad2326d38bd9165485d5f85e936993b3f22c1f
Nick Hennessy <njhnny@gmail.com>
2020-11-02 17:44:26 -0600

    add business logic
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,51 @@
 //Business logic 
+function TravelLog() {
+  this.entry = [];
+  this.currentId = 0;
+}
+
+TravelLog.prototype.addLocation = function(newLocation) {
+  newLocation.id = this.assignId();
+  this.entry.push(newLocation);
+}
+
+TravelLog.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+TravelLog.prototype.findLocation = function(id) {
+  for(let i=0, i< this.entry.length; i ++) {
+    if (this.entry[i]) {
+      if (this.entry[i].id == id) {
+        return this.entry[i];
+      }
+    }
+  };
+  return false
+}
+
+TravelLog.prototype.deleteLocation = function(id) {
+  for(let i=0, i< this.entry.length; i ++) {
+    if (this.entry[i]) {
+      if (this.entry[i].id == id) {
+        delete this.entry[i];
+        return true;
+      }
+    }
+  };
+  return false
+}
+
+function Location(location, date, notes) {
+  this.location = location;
+  this.date = date;
+  this.notes = notes;
+}
 
 // User Interface Logic
+let travelLog = new TravelLog();
+
 $(document).ready(function() {
   $("form#new-location").submit(function(event) {
     event.preventDefault();
@@ -13,9 +58,10 @@ $(document).ready(function() {
     $("input#new-date").val("");
     $("input#new-notes").val("");
     
-    console.log(inputtedLocation);
-    console.log(inputtedDate);
-    console.log(inputtedNotes);
+
+    let newLocation = new Location(inputtedLocation, inputtedDate, inputtedNotes);
+    travelLog.addLocation(newLocation);
+    
   });
 });
 
