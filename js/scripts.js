b3ec76462ea72dcfd102b36bd88a5c53f4eaf55b
Nick Hennessy <njhnny@gmail.com>
2020-11-02 18:32:17 -0600

    add more business logic
    Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ TravelLog.prototype.assignId = function() {
 }
 
 TravelLog.prototype.findLocation = function(id) {
-  for(let i=0, i< this.entry.length; i ++) {
+  for (let i=0; i < this.entry.length; i ++) {
     if (this.entry[i]) {
       if (this.entry[i].id == id) {
         return this.entry[i];
@@ -26,7 +26,7 @@ TravelLog.prototype.findLocation = function(id) {
 }
 
 TravelLog.prototype.deleteLocation = function(id) {
-  for(let i=0, i< this.entry.length; i ++) {
+  for (let i=0; i< this.entry.length; i ++) {
     if (this.entry[i]) {
       if (this.entry[i].id == id) {
         delete this.entry[i];
@@ -46,7 +46,40 @@ function Location(location, date, notes) {
 // User Interface Logic
 let travelLog = new TravelLog();
 
+function displayLocationDetails(travelLogToDisplay) {
+  let travelList = $("ul#travelLog");
+  let htmlForTravelList = "";
+  travelLogToDisplay.entry.forEach(function(newLocation) {
+    htmlForTravelList += "<li id=" + newLocation.id + ">" + newLocation.location + " " + "</li>";
+  });
+  travelList.html(htmlForTravelList);
+}
+
+function showNewLocation(newLocationid) {
+  const contact = addressBook.findContact(contactId);
+  $("#new-location").show();
+  $(".location").html(newLocation.location);
+  $(".date").html(newLocation.date);
+  $(".notes").html(newLocation.notes);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + newLocation.id + ">Delete</button>");
+}
+
+function attachTravelListeners() {
+  $("ul#travelLog").on("click", "li", function() {
+    showNewLocation(this.id);
+  });
+
+  $("#buttons").on("click", ".deleteButton", function() {
+    travelLog.deleteLocation(this.id);
+    $("#show-travelLog").hide();
+    displayLocationDetails(travelLog);
+  });
+};
+
 $(document).ready(function() {
+  attachTravelListeners();
   $("form#new-location").submit(function(event) {
     event.preventDefault();
 
